8c1ef4fc93d17ea6e0e0c05e9769c25583c80f31
Microsoft-Hater <B00164190@mytudublin.ie>
2026-04-26 14:11:24 +0100

    Registered All Models To Admin

--- studentservices/services/admin.py
+++ studentservices/services/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Fee
+from .models import Result
+from .models import Ticket
+
 # Register your models here.
+
+admin.site.register(Fee)
+admin.site.register(Result)
+admin.site.register(Ticket)
